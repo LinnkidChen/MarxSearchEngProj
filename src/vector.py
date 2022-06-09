8c0cc1a5e5d627e04cf740d7348c5ec2ddd43423
彩虹直至黑白 <57987200+781195632@users.noreply.github.com>
2022-06-09 22:42:05 +0800

    Create vector.py

--- src/vector.py
+++ src/vector.py
@@ -1,23 +1,42 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 import json
 import  numpy as np
 import jieba
 
+
+# 按间距中的绿色按钮以运行脚本。
+
 words = input()
-seg_list = jieba.cut_for_search(words)
+seg_list = jieba.lcut_for_search(words)
 size = 0
 vector_word = list()
 for i in seg_list:
     size += 1
     vector_word.append(i)
 print(vector_word)
-vecotor_q = np.zeros(size)
-# print(vecotor_q)
+
+
 with open('./rverIndex.json', 'r', encoding='utf8') as fp:
     json_data = json.load(fp)
+    vecotor_q = np.zeros(len(json_data))
+
+
+mydict = dict()
+
+loc = 0
+for j in json_data:
+    mydict[j] = loc
+    loc += 1
+
+# print(mydict)
 k = 0
+
 while k < size:
     if vector_word[k] in json_data:
-        vecotor_q[k] += 1
+        vecotor_q[mydict[vector_word[k]]] += 1
         k += 1
     else:
         k += 1
